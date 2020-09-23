ceac6142cb0539524cbeb9d3d1b51f5831779cd6
unknown <gangolimufeed@gmail.com>
2020-09-23 13:51:46 +0530

    two camera working

--- src/App.js
+++ src/App.js
@@ -1,30 +1,63 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
+
 
 function App() {
 
+ 
+
    const streamCamVideo =() => {
-    var video = document.querySelector("video");
+    var video = document.getElementById("videoElement");
+   
     navigator.mediaDevices.enumerateDevices()
     .then(devices => {
       console.table(devices)
       var camera = devices.filter(device => device.kind === "videoinput");
-      if (camera) {
-        var constraints = { deviceId: { exact: camera.deviceId } };
+      
+      if (camera[0]) {
+        console.log(camera[0].deviceId)
+       
+        var constraints = { deviceId: { exact: camera[0].deviceId } };
+        
+       //video.srcObject =navigator.mediaDevices.getUserMedia({ video: constraints });
+        //video1.srcObject =navigator.mediaDevices.getUserMedia({ video: constraints1 });
         return navigator.mediaDevices.getUserMedia({ video: constraints });
       }
     })
-    .then(stream =>video.srcObject = stream)
+    .then(stream => video.srcObject = stream)
+    .catch(e => console.error(e));
+  }
+
+  const secondVideo = () =>{
+    var video1 = document.getElementById("videoElement1");
+    navigator.mediaDevices.enumerateDevices()
+    .then(devices => {
+      console.table(devices)
+      var camera = devices.filter(device => device.kind === "videoinput");
+      
+      if (camera) {
+        console.log(camera[1].deviceId)
+        var constraints = { deviceId: { exact: camera[1].deviceId } };
+       
+        
+       //video.srcObject =navigator.mediaDevices.getUserMedia({ video: constraints });
+        //video1.srcObject =navigator.mediaDevices.getUserMedia({ video: constraints1 });
+        return navigator.mediaDevices.getUserMedia({ video: constraints});
+      }
+    })
+    .then(stream => video1.srcObject = stream)
     .catch(e => console.error(e));
   }
 
   return (
     <div className="App">
       <div id="container">
-          <video autoPlay={true} id="videoElement"></video>
+          <video autoPlay={true}  id="videoElement"></video>
+          <video autoPlay={true} id="videoElement1"></video>
         </div>
         <br/>
         <button onClick={streamCamVideo}>Start streaming</button>
+        <button onClick={secondVideo}>Second streaming</button>
     </div>
   );
 }
